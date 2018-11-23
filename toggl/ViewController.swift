59451e497dab58327a79f8a08f3a3b436a5c53d4
Ki-Hyoun Kim @ atmina <atmina@ki-hyouns-macbook-pro.local>
2018-11-23 15:28:55 +0100

    Initial Commit

---
+++ toggl/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  toggl
+//
+//  Created by Ki-Hyoun Kim @ atmina on 23.11.18.
+//  Copyright © 2018 Ki-Hyoun Kim @ atmina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
